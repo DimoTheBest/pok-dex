e2bb11c5ec4e2701f5e37fbc971e7c980b0bf980
DimoTheBest <64138187+DimoTheBest@users.noreply.github.com>
2020-08-04 14:02:43 +0200

    Updates

---
+++ precache-manifest.9046574333eb62667526a69e8b1178a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304c68ee8348d8e116e28ac2119b7d56",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "96caf246477d49edf1a1",
+    "url": "/pokedex-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "958d43cbedbc1daa3ffd",
+    "url": "/pokedex-react/static/css/main.cb9e0640.chunk.css"
+  },
+  {
+    "revision": "96caf246477d49edf1a1",
+    "url": "/pokedex-react/static/js/2.1c6e6ef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex-react/static/js/2.1c6e6ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958d43cbedbc1daa3ffd",
+    "url": "/pokedex-react/static/js/main.c82e063d.chunk.js"
+  },
+  {
+    "revision": "03b4f9a70bc85f50a035",
+    "url": "/pokedex-react/static/js/runtime-main.f28fb264.js"
+  }
+]);
