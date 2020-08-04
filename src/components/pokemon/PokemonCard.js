7dbad29073caba9cd7ab2e7ff4a352f09815393f
DimoTheBest <64138187+DimoTheBest@users.noreply.github.com>
2020-08-04 17:07:36 +0200

    va l'host

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,18 +1,55 @@
 import React, { Component } from "react";
 
+import styled from "styled-components";
+
+const Sprite = styled.img`
+  width: 5em;
+  height: 5em;
+`;
+
 export default class PokemonCard extends Component {
   state = {
     name: "",
     imagUrl: "",
     pokemonIndex: "",
+    imageloading: true,
+    toManyRequests: false,
   };
+  componentDidMount() {
+    const { name, url } = this.props;
+    const pokemonIndex = url.split("/")[url.split("/").length - 2];
+    //const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+    const imageUrl = `https://dmo-s.com/listaPok/${pokemonIndex}.png`;
+
+    this.setState({
+      name: name,
+      imageUrl: imageUrl,
+      pokemonIndex: pokemonIndex,
+    });
+  }
   render() {
-    const name = this.props.name;
-    const url = this.props.url;
     return (
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
-          <div className="card-header">{name}</div>
+          <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <Sprite
+            className="card-img-top rounded mx-auto mt-2"
+            onLoad={() => this.setState({ imageloading: false })}
+            onError={() => this.setState({ toManyRequests: true })}
+            src={this.state.imageUrl}
+          ></Sprite>
+          <div className="card-body mx-auto">
+            <h6 className="card-title">
+              {this.state.name
+                .toLowerCase()
+                .split(" ")
+                .map(
+                  (letter) =>
+                    letter.charAt(0).toUpperCase() + letter.substring(1)
+                )
+                .join("")}
+            </h6>
+          </div>
         </div>
       </div>
     );
